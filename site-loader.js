1d48aa67492be12b48d797d943aac235272f1833
ninjaboy999096 <129236203+ninjaboy999096@users.noreply.github.com>
2025-05-06 17:48:03 -0700

    Update site-loader.js

--- site-loader.js
+++ site-loader.js
@@ -1,4 +1,4 @@
-fetch("https://raw.githubusercontent.com/ninjaboy999096/live-websim-site/refs/heads/main/site.html")
+fetch("https://raw.githubusercontent.com/ninjaboy999096/live-websim-thing/refs/heads/main/site.html")
   .then(res => res.text())
   .then(html => {
     document.body.insertAdjacentHTML("beforeend", html);
